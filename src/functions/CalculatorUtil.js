fcccabfaecdc50962a41363e2ebf30aeb2238e77
xstahol <staffan.holmgren@tre.se>
2019-04-01 21:39:25 +0200

    fixed decimals

--- src/functions/CalculatorUtil.js
+++ src/functions/CalculatorUtil.js
@@ -2,18 +2,18 @@ export const calculateSuggestedDownPayment = (purchaseAmount, cash, moneySavedEa
     const lagfartCost = calculateLagfartCost(purchaseAmount);
     const pantBrevCost = pantBrevNeeded === true ? calculatePantBrevCost(purchaseAmount) : 0;
     const downPayment = cash + calculateMoneySavedUntilPurchase(moneySavedEachMonth, monthsCount) + profitOnSale - moneyLeftAfterPurchase - pantBrevCost - lagfartCost;
-    return downPayment;
+    return Math.floor(downPayment);
 };
 
-export const calculateMinimumDownPayment = (purchaseAmount) => purchaseAmount * (15 / 100);
+export const calculateMinimumDownPayment = (purchaseAmount) => Math.floor(purchaseAmount * (15 / 100));
 
-export const calculateMoneySavedUntilPurchase = (moneySavedEachMonth, monthsCount) => moneySavedEachMonth * monthsCount;
+export const calculateMoneySavedUntilPurchase = (moneySavedEachMonth, monthsCount) => Math.floor(moneySavedEachMonth * monthsCount);
 
-export const calculateLagfartCost = purchaseAmount => purchaseAmount * (1.5 / 100);
+export const calculateLagfartCost = purchaseAmount => Math.ceil(purchaseAmount * (1.5 / 100));
 
-export const calculatePantBrevCost = purchaseAmount => purchaseAmount * (2 / 100);
+export const calculatePantBrevCost = purchaseAmount => Math.ceil(purchaseAmount * (2 / 100));
 
-export const calculateLoanAmount = (purchaseAmount, downPayment) => purchaseAmount - downPayment;
+export const calculateLoanAmount = (purchaseAmount, downPayment) => Math.floor(purchaseAmount - downPayment);
 
 export const calculateLoanQuota = (purchaseAmount, downPayment) => (((purchaseAmount - downPayment) / purchaseAmount) * 100).toFixed(2);
 
