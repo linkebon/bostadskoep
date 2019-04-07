31d2c55e3060fbe547575f35189fce33ba7e837f
xstahol <staffan.holmgren@tre.se>
2019-04-07 20:04:51 +0200

    deploy scripts

--- buildDeployRaspberry.sh
+++ buildDeployRaspberry.sh
@@ -8,12 +8,12 @@ TARGET_SERVER=raspberryhost
 ./build.sh
 echo '########### Deploying to prod ###########'
 echo 'Stopping and removing old app'
-ssh pi@${TARGET_SERVER} "${APP_SCRIPTS_HOME}/STOP_BOSTADSKOEP.sh;rm -rf ${APP_HOME}/${APP_NAME};"
+ssh pi@${TARGET_SERVER} "sudo ${APP_SCRIPTS_HOME}/STOP_BOSTADSKOEP.sh;rm -rf ${APP_HOME}/${APP_NAME};"
 
 echo 'Uploading new version'
 scp -rp build pi@${TARGET_SERVER}:${APP_HOME}
 echo 'Upload done'
-ssh pi@${TARGET_SERVER} "mv ${APP_HOME}/build ${APP_HOME}/${APP_NAME};${APP_SCRIPTS_HOME}/START_BOSTADSKOEP.sh;"
+ssh pi@${TARGET_SERVER} "mv ${APP_HOME}/build ${APP_HOME}/${APP_NAME};sudo ${APP_SCRIPTS_HOME}/START_BOSTADSKOEP.sh;"
 echo '########### Deploying complete ###########'
 
 
