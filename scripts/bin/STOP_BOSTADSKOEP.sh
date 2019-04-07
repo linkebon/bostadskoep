7867a3c1f1480dadefc7d28d5708079c1a7a2cfc
xstahol <staffan.holmgren@tre.se>
2019-04-07 16:07:13 +0200

    start stop scripts on raspberry pi

--- scripts/bin/STOP_BOSTADSKOEP.sh
+++ scripts/bin/STOP_BOSTADSKOEP.sh
@@ -13,5 +13,5 @@ PID=`ps ax --width=1000 | grep node | grep ${APP_NAME} | awk '{print $1}'`
 while [ "$PID"null != "null" ]; do
     sleep 3
     echo "..."
-    PID=`ps ax --width=1000 | grep node | grep bostadskoep | awk '{print $1}'`
+    PID=`ps ax --width=1000 | grep node | grep ${APP_NAME} | awk '{print $1}'`
 done
